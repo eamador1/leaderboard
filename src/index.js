aa69f743d084d494cf18232e590cc3282d0eaf7e
erwin amador <eamador0305@gmail.com>
2023-08-31 21:51:55 -0600

    take out displayList function

--- src/index.js
+++ src/index.js
@@ -1,80 +1,56 @@
 import './style.css';
 
+// POPULATE LIST
+
+import populateList from './module/showList.js';
+
 async function populate() {
-    const requestURL =
-      "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/IV1251GkSsG9dGnKrPGk/scores/";
-    const request = new Request(requestURL);
-  
-    const response = await fetch(request);
-    const allPlayers = await response.json();
-  
-    populateList(allPlayers);
-  }
+  const requestURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/IV1251GkSsG9dGnKrPGk/scores/';
+  const request = new Request(requestURL);
 
-  //POPULATE LIST
+  const response = await fetch(request);
+  const allPlayers = await response.json();
 
-function populateList(obj) {
-  const displayList = document.querySelector(".displayList");
-  const players = obj.result;
-  displayList.innerHTML = '';
-
-  for (const playerData of players) {
-  
-  const playerDiv = document.createElement('div');
-  const user = document.createElement('h5');
-  const score = document.createElement('span');
-
-  playerDiv.classList.add('player');
-
-  user.textContent = `${playerData.user}:`;
-  score.textContent = `${playerData.score}`;
-  
-  playerDiv.appendChild(user);
-  playerDiv.appendChild(score);
-  displayList.appendChild(playerDiv);
-}
+  populateList(allPlayers);
 }
 
 populate();
 
 const form = document.querySelector('.addForm');
 
-form.addEventListener('submit', async event => {
+form.addEventListener('submit', async (event) => {
   event.preventDefault();
 
   const user = form.querySelector('[name="user"]').value;
   const score = form.querySelector('[name="score"]').value;
 
   try {
-    const res = await fetch(
+    await fetch(
       'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/IV1251GkSsG9dGnKrPGk/scores/',
       {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json; charset=UTF-8' ,
-          },
-        body: JSON.stringify({user:user, score:score}),
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: JSON.stringify({ user, score }),
       },
     );
 
-    const resData = await res.json();
+    const emptyInputText = () => {
+      const user = form.querySelector('[name="user"]');
+      const score = form.querySelector('[name="score"]');
+      user.value = '';
+      score.value = '';
+    };
 
-    emptyInputText()
-
+    emptyInputText();
   } catch (err) {
-    console.error(err.message);
+    return err;
   }
+  return {};
 });
 
-const emptyInputText = () => {
-  const user = form.querySelector('[name="user"]');
-  const score = form.querySelector('[name="score"]');
-  user.value = '';
-  score.value= '';
-};
-
 const refresh = document.querySelector('.refresh');
-refresh.addEventListener('click', async event => {
-
-await populate();
+refresh.addEventListener('click', async () => {
+  await populate();
 });
