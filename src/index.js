4ffbe2914db43744a148fefa94591eb73e668259
erwin amador <eamador0305@gmail.com>
2023-08-31 19:11:36 -0600

    Add functionality to submit and refresh buttons

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ async function populate() {
 function populateList(obj) {
   const displayList = document.querySelector(".displayList");
   const players = obj.result;
+  displayList.innerHTML = '';
 
   for (const playerData of players) {
   
@@ -35,3 +36,45 @@ function populateList(obj) {
 }
 
 populate();
+
+const form = document.querySelector('.addForm');
+
+form.addEventListener('submit', async event => {
+  event.preventDefault();
+
+  const user = form.querySelector('[name="user"]').value;
+  const score = form.querySelector('[name="score"]').value;
+
+  try {
+    const res = await fetch(
+      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/IV1251GkSsG9dGnKrPGk/scores/',
+      {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8' ,
+          },
+        body: JSON.stringify({user:user, score:score}),
+      },
+    );
+
+    const resData = await res.json();
+
+    emptyInputText()
+
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+const emptyInputText = () => {
+  const user = form.querySelector('[name="user"]');
+  const score = form.querySelector('[name="score"]');
+  user.value = '';
+  score.value= '';
+};
+
+const refresh = document.querySelector('.refresh');
+refresh.addEventListener('click', async event => {
+
+await populate();
+});
